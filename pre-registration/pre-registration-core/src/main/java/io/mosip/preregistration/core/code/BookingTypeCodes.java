214da907f686a1ccd42bf3db4cbc45e62dbdeb92
Shiba <karthik.s@technoforte.co.in>
2025-11-10 16:57:41 +0530

    alien lost

--- pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/code/BookingTypeCodes.java
+++ pre-registration/pre-registration-core/src/main/java/io/mosip/preregistration/core/code/BookingTypeCodes.java
@@ -14,7 +14,9 @@ public enum BookingTypeCodes {
 
 	ALIENNEW("ALIENNEW"),
 
-	ALIENRENEWAL("ALIENRENEWAL");
+	ALIENRENEWAL("ALIENRENEWAL"),
+
+	ALIENLOST("ALIENLOST");
 
 	BookingTypeCodes(String bookingTypeCode) {
 		this.bookingTypeCode = bookingTypeCode;
